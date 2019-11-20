7aca9ddac05e8a07dd7794d686d5783d7728e71f
Jan Hovland <jan.hovland@lyse-net>
2019-11-20 12:58:19 +0100

    Update

---
+++ PersonOverView/ToSignInView.swift
@@ -0,0 +1,13 @@
+//
+//  ToSignInView.swift
+//  PersonOverView
+//
+//  Created by Jan Hovland on 20/11/2019.
+//  Copyright © 2019 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+func toSignInView() -> some View {
+    SignInView()
+}
