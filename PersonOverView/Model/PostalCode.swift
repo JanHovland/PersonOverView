1218b5c5cec7be6d80e8bfdf29b319c5e9379502
Jan Hovland <jan.hovland@lyse.net>
2020-02-09 20:40:17 +0100

    Update

--- PersonOverView/Model/PostalCode.swift
+++ PersonOverView/Model/PostalCode.swift
@@ -9,41 +9,12 @@
 import SwiftUI
 import CloudKit
 
-//struct PostalCode: Identifiable {
-//   var id = UUID()
-//   var recordID: CKRecord.ID?
-//   var postalCode: String = ""
-//   var postalName: String = ""
-//   var municipalityNumber: String = ""
-//   var municipalityName: String = ""
-//   var categori: String = ""
-//}
-
-struct PostalCode {
-    var postalCode: String
-    var postalName: String
-    var municipalityNumber: String
-    var municipalityName: String
-    var categori: String
-
-    init(postalCode: String,
-         postalName: String,
-         municipalityNumber: String,
-         municipalityName: String,
-         categori: String) {
-
-        self.postalCode = postalCode
-        self.postalName = postalName
-        self.municipalityNumber = municipalityNumber
-        self.municipalityName = municipalityName
-        self.categori = categori
-    }
-
-    init() {
-        self.init(postalCode: "",
-                  postalName: "",
-                  municipalityNumber: "",
-                  municipalityName: "",
-                  categori: "")
-    }
+struct PostalCode: Identifiable {
+   var id = UUID()
+   var recordID: CKRecord.ID?
+   var postalCode: String = ""
+   var postalName: String = ""
+   var municipalityNumber: String = ""
+   var municipalityName: String = ""
+   var categori: String = ""
 }
