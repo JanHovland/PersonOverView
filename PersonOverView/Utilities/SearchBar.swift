766c390d1c92208960d5a876a1a2210b24d09181
Jan Hovland <jan.hovland@lyse.net>
2020-02-12 20:27:32 +0100

    Update

---
+++ PersonOverView/Utilities/SearchBar.swift
@@ -0,0 +1,38 @@
+//
+//  SearchBar.swift
+//  PersonOverView
+//
+//  Created by Jan Hovland on 12/02/2020.
+//  Copyright © 2020 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+/// SearchBar er ikke er ikke en dekl av SWIFTUI
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var text: String
+        init(text: Binding<String>) {
+            _text = text
+        }
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.placeholder = NSLocalizedString("Search for PostalCode...", comment: "FindPostalCode")
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
+
+
