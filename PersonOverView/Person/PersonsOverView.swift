091907f16895859decdc35816ccb88e9654bca7a
Jan Hovland <jan.hovland@lyse.net>
2020-03-24 10:05:44 +0100

    Update

--- PersonOverView/Person/PersonsOverView.swift
+++ PersonOverView/Person/PersonsOverView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 import CloudKit
+import Foundation
+import MapKit
 
 struct PersonsOverView: View {
 
@@ -28,6 +30,7 @@ struct PersonsOverView: View {
     @State private var indexSetDelete = IndexSet()
     @State private var recordID: CKRecord.ID?
 
+
     var body: some View {
         NavigationView {
             VStack {
@@ -40,7 +43,26 @@ struct PersonsOverView: View {
                         $0.lastName.localizedStandardContains (self.searchText)    })) {
                             person in
                             NavigationLink(destination: PersonView(person: person)) {
-                                ShowPersons(person: person)
+                                VStack(alignment: .leading) {
+                                    ShowPersons(person: person)
+                                    HStack(alignment: .center, spacing: 42.5) {
+                                        /// For å få vist iconene uten en space nedenfor dem, måtte jeg legge inn Text("  ")
+                                        Text("  ")
+                                        Image("map")
+                                            .resizable()
+                                            .frame(width: 46, height: 46, alignment: .center)
+                                        Image("phone")
+                                            .resizable()
+                                            .frame(width: 40, height: 40, alignment: .center)
+                                        Image("message")
+                                            .resizable()
+                                            .frame(width: 40, height: 40, alignment: .center)
+
+                                        Image("mail")
+                                            .resizable()
+                                            .frame(width: 46, height: 46, alignment: .center)
+                                    }
+                                }
                             }
                     }
                         /// Sletter  valgt person og oppdaterer CliudKit
@@ -166,6 +188,9 @@ struct PersonsOverView: View {
 
 /// Et eget View for å vise person detail view
 struct ShowPersons: View {
+
+    @State private var showMap = false
+
     static let taskDateFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -173,48 +198,113 @@ struct ShowPersons: View {
     }()
     var person: Person
     var body: some View {
-        HStack(spacing: 10) {
-            if person.image != nil {
-                Image(uiImage: person.image!)
-                    .resizable()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 1))
-            } else {
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .font(.system(size: 16, weight: .ultraLight))
-                    .frame(width: 50, height: 50, alignment: .center)
-            }
-            VStack (alignment: .leading, spacing: 5) {
-                HStack {
-                    /// Sletter første character i fornavnet dersom den er en emoji
-                    TextDeleteFirstEmoji(firstName : person.firstName)
-                        .font(Font.title.weight(.ultraLight))
-                    Text(person.lastName)
-                        .font(Font.body.weight(.ultraLight))
+        VStack {
+            HStack {
+                if person.image != nil {
+                    Image(uiImage: person.image!)
+                        .resizable()
+                        .frame(width: 60, height: 60, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                } else {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .font(.system(size: 16, weight: .ultraLight))
+                        .frame(width: 60, height: 60, alignment: .center)
                 }
+                VStack (alignment: .leading, spacing: 5) {
+                    HStack {
+                        /// Sletter første character i fornavnet dersom den er en emoji
+                        TextDeleteFirstEmoji(firstName : person.firstName)
+                            .font(Font.title.weight(.ultraLight))
+                        Text(person.lastName)
+                            .font(Font.body.weight(.ultraLight))
+                    }
                     .font(Font.body.weight(.ultraLight))
-                Text("\(person.dateOfBirth, formatter: Self.taskDateFormat)")
-                    .font(.custom("system", size: 17))
-                HStack {
-                    Text(person.address)
+                    Text("\(person.dateOfBirth, formatter: Self.taskDateFormat)")
                         .font(.custom("system", size: 17))
+                    HStack {
+                        Text(person.address)
+                            .font(.custom("system", size: 17))
+                    }
+                    HStack {
+                        Text(person.cityNumber)
+                        Text(person.city)
+                    }
+                    .font(.custom("system", size: 17))
                 }
-                HStack {
-                    Text(person.cityNumber)
-                    Text(person.city)
-                }
-                .font(.custom("system", size: 17))
             }
-            .padding(.top, 10)
         }
-            /// Ta bort tastaturet når en klikker utenfor feltet
-            .modifier(DismissingKeyboard())
-            /// Flytte opp feltene slik at keyboard ikke skjuler aktuelt felt
-            .modifier(AdaptsToSoftwareKeyboard())
+        /// Ta bort tastaturet når en klikker utenfor feltet
+        .modifier(DismissingKeyboard())
+        /// Flytte opp feltene slik at keyboard ikke skjuler aktuelt felt
+        .modifier(AdaptsToSoftwareKeyboard())
     }
 }
+
+struct MapView: UIViewRepresentable {
+
+    var locationOnMap: String
+    var address: String
+    var subtitle: String
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        /// Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(locationOnMap, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let placemarks = placemarks {
+                /// Get the first placemark
+                let placemark = placemarks[0]
+                /// placemark = Uelandsgata 2, Uelandsgata 2, 4360 Varhaug, Norge @ <+58.61729080,+5.64474960> +/- 100.00m, region CLCircularRegion (identifier:'<+58.61729080,+5.64474960> radius 70.82', center:<+58.61729080,+5.64474960>, radius:70.82m)
+                /// Add annotation
+                let annotation = MKPointAnnotation()
+                mapView.addAnnotation(annotation)
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    annotation.title = self.address
+                    annotation.subtitle = self.subtitle
+                    /// Display the annotationn
+                    mapView.showAnnotations([annotation], animated: true)
+                    mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+
+        })
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
+
+}
+
+
 
 func TextDeleteFirstEmoji(firstName : String) -> some View {
     let index0 = firstName.index(firstName.startIndex, offsetBy: 0)
