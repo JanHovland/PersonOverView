7d348e20cbdeb03d9cbb6e1553cd546d81e7cacd
Jan Hovland <jan.hovland@lyse.net>
2020-03-24 17:14:43 +0100

    Update

--- PersonOverView/Person/PersonMapView.swift
+++ PersonOverView/Person/PersonMapView.swift
@@ -16,13 +16,11 @@ import MapKit
 
 struct PersonMapView: View {
 
-//    var locationOnMap: String = "Uelandsgata 2 4360 Varhaug"
-//    var address: String = "Uelandsgata 2"
-//    var subtitle: String = "4360 Varhaug"
+    @Environment(\.presentationMode) var presentationMode
 
-    var locationOnMap: String = "Sentrumsvegen 17 3539 Flå"
-    var address: String = "Sentrumsvegen 17"
-    var subtitle: String = "3539 Flå"
+    var locationOnMap: String
+    var address: String
+    var subtitle: String
 
     var body: some View {
         /// Compilerer OK men under kjøring kommer denne feilmeldingen:
@@ -31,68 +29,85 @@ struct PersonMapView: View {
         MapView(locationOnMap: locationOnMap,
                 address: address,
                 subtitle: subtitle)
+            .overlay(
+                HStack {
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }, label: {
+                            Image(systemName: "chevron.down.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.none)
+                        })
+                            .padding(.trailing, 20)
+                            .padding(.top, 70)
+                        Spacer()
+                    }
+            })
     }
+
 }
 
-//struct MapView: UIViewRepresentable {
-//
-//    var locationOnMap: String
-//    var address: String
-//    var subtitle: String
-//
-//    func makeUIView(context: Context) -> MKMapView {
-//        let mapView = MKMapView()
-//        mapView.delegate = context.coordinator
-//        /// Convert address to coordinate and annotate it on map
-//        let geoCoder = CLGeocoder()
-//        geoCoder.geocodeAddressString(locationOnMap, completionHandler: { placemarks, error in
-//            if let error = error {
-//                print(error.localizedDescription)
-//                return
-//            }
-//            if let placemarks = placemarks {
-//                /// Get the first placemark
-//                let placemark = placemarks[0]
-//                /// placemark = Uelandsgata 2, Uelandsgata 2, 4360 Varhaug, Norge @ <+58.61729080,+5.64474960> +/- 100.00m, region CLCircularRegion (identifier:'<+58.61729080,+5.64474960> radius 70.82', center:<+58.61729080,+5.64474960>, radius:70.82m)
-//                /// Add annotation
-//                let annotation = MKPointAnnotation()
-//                mapView.addAnnotation(annotation)
-//                if let location = placemark.location {
-//                    annotation.coordinate = location.coordinate
-//                    annotation.title = self.address
-//                    annotation.subtitle = self.subtitle
-//                    /// Display the annotationn
-//                    mapView.showAnnotations([annotation], animated: true)
-//                    mapView.selectAnnotation(annotation, animated: true)
-//                }
-//            }
-//
-//        })
-//        return mapView
-//    }
-//
-//    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-//
-//    }
-//
-//    func makeCoordinator() -> Coordinator {
-//        Coordinator(self)
-//    }
-//
-//    class Coordinator: NSObject, MKMapViewDelegate {
-//        var parent: MapView
-//        init(_ parent: MapView) {
-//            self.parent = parent
-//        }
-//        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-//            print(mapView.centerCoordinate)
-//        }
-//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-//            view.canShowCallout = true
-//            return view
-//        }
-//    }
-//
-//}
+struct MapView: UIViewRepresentable {
+
+    var locationOnMap: String
+    var address: String
+    var subtitle: String
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        /// Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(locationOnMap, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let placemarks = placemarks {
+                /// Get the first placemark
+                let placemark = placemarks[0]
+                /// placemark = Uelandsgata 2, Uelandsgata 2, 4360 Varhaug, Norge @ <+58.61729080,+5.64474960> +/- 100.00m, region CLCircularRegion (identifier:'<+58.61729080,+5.64474960> radius 70.82', center:<+58.61729080,+5.64474960>, radius:70.82m)
+                /// Add annotation
+                let annotation = MKPointAnnotation()
+                mapView.addAnnotation(annotation)
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    annotation.title = self.address
+                    annotation.subtitle = self.subtitle
+                    /// Display the annotationn
+                    mapView.showAnnotations([annotation], animated: true)
+                    mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+
+        })
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
+
+}
 
--- PersonOverView/Person/PersonsOverView.swift
+++ PersonOverView/Person/PersonsOverView.swift
@@ -32,8 +32,6 @@ struct PersonsOverView: View {
 
     @State private var showMap = false
 
-
-
     var body: some View {
         NavigationView {
             VStack {
@@ -159,15 +157,21 @@ struct PersonsOverView: View {
 
 /// Et eget View for å vise person detail view
 struct ShowPersons: View {
-
-    @State private var showMap = false
+    var person: Person
 
     static let taskDateFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         return formatter
     }()
-    var person: Person
+
+    @State private var showMap = false
+    @State private var locationOnMap: String = ""
+    @State private var address: String = ""
+    @State private var subtitle: String = ""
+
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
         VStack (alignment: .leading) {
             HStack {
@@ -213,8 +217,9 @@ struct ShowPersons: View {
                     .frame(width: 46, height: 46, alignment: .center)
                     .gesture(
                         TapGesture()
-                            .onEnded({
+                            .onEnded({_ in
                                 print("Map tapped")
+                                self.showMap.toggle()
                             })
                     )
                 Image("phone")
@@ -247,76 +252,33 @@ struct ShowPersons: View {
             }
 
         }
+        .sheet(isPresented: $showMap) {
+            PersonMapView(locationOnMap: self.person.address + " " + self.person.cityNumber + " " + self.person.city,
+                          address: self.person.address,
+                          subtitle: self.person.cityNumber + " " + self.person.city)
+        }
         /// Ta bort tastaturet når en klikker utenfor feltet
         .modifier(DismissingKeyboard())
         /// Flytte opp feltene slik at keyboard ikke skjuler aktuelt felt
         .modifier(AdaptsToSoftwareKeyboard())
-    }
-}
-
-struct MapView: UIViewRepresentable {
-
-    var locationOnMap: String
-    var address: String
-    var subtitle: String
-
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        mapView.delegate = context.coordinator
-        /// Convert address to coordinate and annotate it on map
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString(locationOnMap, completionHandler: { placemarks, error in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            if let placemarks = placemarks {
-                /// Get the first placemark
-                let placemark = placemarks[0]
-                /// placemark = Uelandsgata 2, Uelandsgata 2, 4360 Varhaug, Norge @ <+58.61729080,+5.64474960> +/- 100.00m, region CLCircularRegion (identifier:'<+58.61729080,+5.64474960> radius 70.82', center:<+58.61729080,+5.64474960>, radius:70.82m)
-                /// Add annotation
-                let annotation = MKPointAnnotation()
-                mapView.addAnnotation(annotation)
-                if let location = placemark.location {
-                    annotation.coordinate = location.coordinate
-                    annotation.title = self.address
-                    annotation.subtitle = self.subtitle
-                    /// Display the annotationn
-                    mapView.showAnnotations([annotation], animated: true)
-                    mapView.selectAnnotation(annotation, animated: true)
+        .overlay(
+            HStack {
+                Spacer()
+                VStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "chevron.down.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.none)
+                    })
+                        .padding(.trailing, 20)
+                        .padding(.top, 70)
+                    Spacer()
                 }
-            }
-
         })
-        return mapView
     }
-
-    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
-        init(_ parent: MapView) {
-            self.parent = parent
-        }
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            print(mapView.centerCoordinate)
-        }
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            view.canShowCallout = true
-            return view
-        }
-    }
-
 }
-
-
 
 func TextDeleteFirstEmoji(firstName : String) -> some View {
     let index0 = firstName.index(firstName.startIndex, offsetBy: 0)
